070287e9595ad1365a78a50f89c4e3167fcfafb7
Andrew Murray <darbyguitars@gmail.com>
2020-01-07 11:32:49 -0600

    set up Apollo server within express on localhost:8080 	modified:   server.js

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
 const PORT = 8080
 
 let app = express()
 
-app.listen(PORT, () => {
-    console.log(`
+const typeDefs = gql`
+	type Query {
+		hello: String
+	}
+`
+
+const resolvers = {
+	Query: {
+		hello: () => 'Hello Word'
+	}
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+//set up with express
+server.applyMiddleware({ app })
+
+app.listen({ port : PORT}, (url) => {
+	console.log(`
     YOUR SERVER IS LISTENING ON PORT: ${PORT}
+    YOUR GQL PLAYGROUND IS RUNNING AT : http://localhost:${PORT}${server.graphqlPath}
     `)
 })
